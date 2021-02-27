d1da8aae87e9e52b4a0d081df2486e72c93625fb
Marcin Kufel <marcinkufel84@gmail.com>
2021-02-27 20:45:56 +0100

    add function toggleHideDone to button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
+import React from 'react';
 import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
 import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
+import { useState } from "react";
 
 const tasks = [
   { id: 1, content: "switch to React", done: false },
   { id: 2, content: "study more and more", done: true },
 ];
 
-const hideDone = false;
 
 function App() {
+  const [hideDone, setHideDone] = useState(false);
+
+  const toggleHideDone = () => {
+    setHideDone(hideDone => !hideDone);
+  };
   return (
     <Container>
       <Header title="To do list" />
@@ -24,7 +30,12 @@ function App() {
       <Section
         title="To do list"
         body={<Tasks tasks={tasks} hideDone={hideDone} />}
-        extraHeaderContent={<Buttons tasks={tasks} hideDone={hideDone} />}
+        extraHeaderContent={
+          <Buttons
+            tasks={tasks}
+            hideDone={hideDone}
+            toggleHideDone={toggleHideDone}
+          />}
       />
 
     </Container>
